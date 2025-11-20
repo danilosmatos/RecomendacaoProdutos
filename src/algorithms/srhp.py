f8fea4280ac171a41ce6e05c73262aa570ee86d0
Danilo <danilosoaresmatos@gmail.com>
2025-11-20 15:24:58 -0300

    começo da arvore do srph, falta uma classe para a arvore em si, ou, caso for melhor, juntar a  classe nó na arvore junto dos metodos de adicionar e etc

---
+++ src/algorithms/srhp.py
@@ -0,0 +1,9 @@
+# no_avl.py
+import time
+
+class No_SRPH:
+    def __init__(self, dado):
+        self.dado = dado
+        self.altura = 1  # Altura inicial do nó é 1
+        self.esquerda = None
+        self.direita = None
