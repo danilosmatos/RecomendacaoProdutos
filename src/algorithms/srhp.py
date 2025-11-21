042b0a2c0eadd28735995092fd54bb6e66aa2a3f
Danilo <danilosoaresmatos@gmail.com>
2025-11-21 15:45:49 -0300

    tirei o srhp do main para mover para o dev, criei o main

--- src/algorithms/srhp.py
+++
@@ -1,6 +0,0 @@
-class No_SRPH:
-    def __init__(self, dado):
-        self.dado = dado
-        self.altura = 1  # Altura inicial do nó é 1
-        self.esquerda = None
-        self.direita = None
